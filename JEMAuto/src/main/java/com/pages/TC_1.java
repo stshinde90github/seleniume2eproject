865a46e49a2849344e805c99bacac27f83db353d
Santosh Shinde <jshinds4@jci.com>
2021-08-14 14:39:00 +0530

    adding TC_1 to repo

---
+++ JEMAuto/src/main/java/com/pages/TC_1.java
@@ -0,0 +1,12 @@
+package com.pages;
+
+public class TC_1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		System.out.println("to add in to git hub");
+
+	}
+
+}
